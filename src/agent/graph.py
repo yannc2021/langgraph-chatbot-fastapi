71a30560750890dcc7930d3bde91e2404a373ec6
yannc2021 <yannc2021@gmail.com>
2025-05-20 09:01:00 +0800

    first commit

---
+++ src/agent/graph.py
@@ -0,0 +1,50 @@
+from typing import Any, TypedDict
+
+#from langchain.chat_models import init_chat_model
+from langchain_core.messages import AnyMessage
+from langchain_core.messages.utils import count_tokens_approximately
+from langgraph.graph import StateGraph, START, MessagesState
+from langgraph.checkpoint.memory import InMemorySaver
+from langmem.short_term import SummarizationNode
+from langchain_openai import ChatOpenAI
+
+model = ChatOpenAI(model="gpt-4o")
+summarization_model = model.bind(max_tokens=256)
+
+class State(MessagesState):
+    context: dict[str, Any]
+
+class LLMInputState(TypedDict):
+    summarized_messages: list[AnyMessage]
+    context: dict[str, Any]
+
+summarization_node = SummarizationNode(
+    token_counter=count_tokens_approximately,
+    model=summarization_model,
+    max_tokens=512,
+    max_tokens_before_summary=256,
+    max_summary_tokens=256,
+)
+
+
+def call_model(state: LLMInputState):
+    response = model.invoke(state["summarized_messages"])
+    return {"messages": [response]}
+
+checkpointer = InMemorySaver()
+builder = StateGraph(State)
+builder.add_node(call_model)
+builder.add_node("summarize", summarization_node)
+builder.add_edge(START, "summarize")
+builder.add_edge("summarize", "call_model")
+graph = builder.compile(checkpointer=checkpointer)
+
+# Invoke the graph
+config = {"configurable": {"thread_id": "1"}}
+graph.invoke({"messages": "hi, my name is alice", "thread_id": "1"}, config)
+graph.invoke({"messages": "write a short poem about cats", "thread_id": "1"}, config)
+graph.invoke({"messages": "now do the same but for dogs", "thread_id": "1"}, config)
+final_response = graph.invoke({"messages": "what's my name?", "thread_id": "1"}, config)
+
+final_response["messages"][-1].pretty_print()
+print("\nSummary:", final_response["context"]["running_summary"].summary)
